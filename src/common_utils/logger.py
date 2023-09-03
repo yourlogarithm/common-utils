0c2b857a32b719c56ab97e2bc207bf019fcd87dd
Vlad-Sebastian Cretu <vlad.sebastian.cretu.dev@gmail.com>
2023-09-04 00:54:06 +0300

    set default to warning

--- src/common_utils/logger.py
+++ src/common_utils/logger.py
@@ -4,7 +4,7 @@ import logging
 class Logger:
     def __init__(self, name: str, level: str = 'INFO'):
         logging.basicConfig(
-            level=level,
+            level=logging.WARNING,
             format="%(asctime)s [%(levelname)s] %(message)s",
             handlers=[
                 logging.FileHandler('debug.log'),
@@ -12,6 +12,7 @@ class Logger:
             ]
         )
         self._inner = logging.getLogger(name)
+        self._inner.setLevel(level.upper())
 
     def __getattr__(self, item):
         return getattr(self._inner, item)
