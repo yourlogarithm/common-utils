018b9b40ff11c1727d7b711b37f55350582eace6
Vlad-Sebastian Cretu <vlad.sebastian.cretu.dev@gmail.com>
2023-09-03 23:12:37 +0300

    fixed not executing inner function

--- src/common_utils/__init__.py
+++ src/common_utils/__init__.py
@@ -8,4 +8,4 @@ from retry_async.api import EXCEPTIONS
 async def persistent_execution(func: Callable, *, exceptions: EXCEPTIONS = Exception, tries: int = -1, delay: float = 0, max_delay: float = None,
                                backoff: float = 1, logger_: logging.Logger, **kwargs):
     decorated_async_function = retry(is_async=True, exceptions=exceptions, tries=tries, delay=delay, max_delay=max_delay, backoff=backoff, logger=logger_)
-    return decorated_async_function(func, **kwargs)
+    return await decorated_async_function(func, **kwargs)()
